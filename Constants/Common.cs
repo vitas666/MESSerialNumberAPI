bddd1798eeca248d476f763ff0b82524c21c4f8d
Vitas <twandy543hient@gmail.com>
2025-01-06 20:39:22 +0800

    Create Common.cs

---
+++ Constants/Common.cs
@@ -0,0 +1,30 @@
+﻿namespace MESSerialNumberAPI.Constant
+{
+    public class Common
+    {
+        public const string APP_SETTINGS_KEY_TEST_URL_AUTHORITY = "TestURLAuthority";
+
+        public const char COMMA = ',';
+
+        public const string UPDATEDATETIME = "UPDATEDATETIME";
+        public const string Y = "Y";
+        public const string N = "N";
+        public const string MESSAGE_CODE = "MESSAGE_CODE";
+        public const string SUCCESS = "SUCCESS";
+        public const string FAIL = "FAIL";
+        public const string Exception = "Exception";
+        public const string DESH = "-";
+        public const string TRIPLE_DESH = "---";
+        public const string POST = "POST";
+        public const string GET = "GET";
+        public const string PUT = "PUT";
+        public const string APP_JSON = "application/json";
+        public const string CIMESDB = "CiMESDB";
+        public const string MESSNDB = "MESSNDB";
+        public const string CiMESDB = "CiMESDB";
+        public const string ScrmKeySerialHist = "TBKEY_SerialHist@MES";
+        public const string ScrmKeySerialMast = "TBKEY_SerialMast@MES";
+        public const int batchSize = 20;
+    }
+}
+
